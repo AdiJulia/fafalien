10fa2bf746841e56a40486895cf006a6a021fb98
AdiJulia <adiadijulia@gmail.com>
2025-08-23 15:16:26 +0700

    Commit cek kotak 1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let errorIndex = 0;
+
 function checkLogin() {
   const username = document.getElementById("username").value.toLowerCase();
   const password = document.getElementById("password").value;
@@ -11,19 +13,29 @@ function checkLogin() {
     document.getElementById("continueBtn").addEventListener("click", () => {
       window.location.href = "dashboard.html";
     });
+  } else if (username != "love") {
+    const messages = ["Anomali ini di hp ku username-nya siapa joo?"];
+    errorMessage.textContent =
+      messages[Math.floor(Math.random() * messages.length)];
+    errorMessage.classList.remove("hidden");
+  } else if (username === "love" && password === "04052025") {
+    const messages = ["AYO SAYANGKU DIKIT LAGI KAMU BISA!!! 😃"];
+    errorMessage.textContent =
+      messages[Math.floor(Math.random() * messages.length)];
+    errorMessage.classList.remove("hidden");
   } else {
     const messages = [
       "Dikit lagiii boi!!!",
       "Apa kamu sayangku??",
-      "Anomali ini di hp ku namanya siapa wak?",
-      "Meski salah, tetapi falola tetep dihati gw joooo",
-      "Kira-kira apa yaa?",
-      "Inget sesuatu ga soal password",
+      "Meski salah, tetapi falola tetep dihati gw joooo❤",
+      "Inget sesuatu ga soal password?",
     ];
 
-    errorMessage.textContent =
-      messages[Math.floor(Math.random() * messages.length)];
+    errorMessage.textContent = messages[errorIndex];
     errorMessage.classList.remove("hidden");
+
+    // geser ke pesan berikutnya, tapi kalau sudah terakhir tetap di situ
+    errorIndex = (errorIndex + 1) % messages.length;
   }
 }
 
@@ -33,3 +45,5 @@ document.getElementById("password").addEventListener("keypress", function (e) {
     checkLogin();
   }
 });
+
+
